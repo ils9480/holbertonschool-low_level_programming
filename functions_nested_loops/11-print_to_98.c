9a465dfa6a4f6ed4684a6492950b3b1b4d374d98
ils9480 <8952@holbertonstudents.com>
2024-02-23 01:23:47 -0800

    Functions, nested loops

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,38 +1,31 @@
-#include "main.h"
 #include <stdio.h>
 
-/**
- * print_to_98 - Function that prints all natural numbers
- *               from n to 98, followed by a new line
- * @n: natural number
- */
+void print_to_98(int n) {
+    // Check if n is greater than or equal to 98
+    if (n >= 98) {
+        // Print numbers from n to 98 decreasingly
+        for (int i = n; i >= 98; i--) {
+            printf("%d", i);
+            if (i != 98) {
+                printf(", ");
+            }
+        }
+    } else {
+        // Print numbers from n to 98 increasingly
+        for (int i = n; i <= 98; i++) {
+            printf("%d", i);
+            if (i != 98) {
+                printf(", ");
+            }
+        }
+    }
+    printf("\n");
+}
 
-void print_to_98(int n)
-{
-	int i;
-
-	i = n;
-
-	if (n < 98)
-	{
-		for (; i <= 98; i++)
-		{
-			printf("%d, ", i);
-		}
-
-		_putchar('\n');
-	}
-	else if (n == 98)
-	{
-		printf("%d\n", n);
-	}
-	else
-	{
-		for (; i <= 98; i--)
-		{
-			printf("%d, ", i);
-		}
-
-		_putchar('\n');
-	}
+int main() {
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    print_to_98(n);
+    return 0;
 }
