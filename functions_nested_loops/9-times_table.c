5fb04d9809bc6d6e4fdd1b436b8a33f703b2aa82
ils9480 <8952@holbertonstudents.com>
2024-02-23 01:53:18 -0800

    Functions, nested loops

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,13 +1,45 @@
-/* Déclaration de la fonction times_table */
-void times_table(void) {
-    int multiplicateur = 0;
-    int resultat;
+#include "main.h"
 
-    printf("Table de multiplication de 9 :\n");
+/**
+ * times_table - entry point
+ *
+ * Description: prints the 9 times table
+ *
+ * Return: 0 for success.
+ */
 
-    while (multiplicateur <= 10) {
-        resultat = 9 * multiplicateur;
-        printf("9 * %d = %d\n", multiplicateur, resultat);
-        multiplicateur++;
-    }
+void times_table(void)
+{
+	int n;
+	int i;
+	int result;
+	int a;
+	int b;
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (i = 0; i <= 9; i++)
+		{
+			result = n * i;
+			if (result > 9)
+			{
+				a = result % 10;
+				b = (result - a) / 10;
+				_putchar(44);
+				_putchar(32);
+				_putchar(b + '0');
+				_putchar(a + '0');
+			} else
+			{
+				if (i != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+				 _putchar(result + '0');
+			}
+		}
+		 _putchar('\n');
+	}
 }
